cfbca2929f808406597e93a0fb879e88fa0afc84
Ouroboros42 <lucas.gathercole@gmail.com>
2024-08-12 15:28:20 +0100

    Include contextmanager based app object for easy use

---
+++ dowser/app.py
@@ -0,0 +1,82 @@
+from . import Root
+import cherrypy
+import logging
+import logging.config
+
+class MemoryApp:
+    def __init__(self, port: int):
+        self.port = port
+        logging.config.dictConfig(self.LOG_CONF)
+
+    def __enter__(self):
+        self.root = Root()
+
+        cherrypy.tree.mount(self.root)
+        cherrypy.config.update({
+            'environment': 'embedded',
+            'server.socket_port': self.port,
+            'log.screen': False,
+            'log.access_file': '',
+            'log.error_file': ''
+        })
+    
+        cherrypy.engine.start()
+
+    def __exit__(self, *args):
+        cherrypy.engine.exit()
+
+    LOG_CONF = {
+        'version': 1,
+
+        'formatters': {
+            'void': {
+                'format': ''
+            },
+            'standard': {
+                'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+            },
+        },
+
+        'handlers': {
+            'cherrypy_console': {
+                'level':'WARNING',
+                'class':'logging.StreamHandler',
+                'formatter': 'void',
+                'stream': 'ext://sys.stdout'
+            },
+            
+            'cherrypy_access': {
+                'level':'WARNING',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'formatter': 'void',
+                'filename': 'access.log',
+                'maxBytes': 10485760,
+                'backupCount': 20,
+                'encoding': 'utf8'
+            },
+
+            'cherrypy_error': {
+                'level':'WARNING',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'formatter': 'void',
+                'filename': 'errors.log',
+                'maxBytes': 10485760,
+                'backupCount': 20,
+                'encoding': 'utf8'
+            },
+        },
+
+        'loggers': {
+            'cherrypy.access': {
+                'handlers': ['cherrypy_access'],
+                'level': 'INFO',
+                'propagate': False
+            },
+            
+            'cherrypy.error': {
+                'handlers': ['cherrypy_console', 'cherrypy_error'],
+                'level': 'INFO',
+                'propagate': False
+            },
+        }
+    }
